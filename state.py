7ac12a7acea53aeeef8a28318020a4d6e72c778c
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2020-03-14 23:38:08 +0300

    refactoring of state.py

--- state.py
+++ state.py
@@ -10,88 +10,80 @@ from typing import List
 
 
 class State:
-	"""State of game."""
-	_TOWERS_COUNT = 3
-	_TOWERS_MARGIN = 1  # distance between towers
+    """State of game."""
+    _TOWERS_COUNT = 3
+    _TOWERS_MARGIN = 2  # distance between towers
 
-	class Layer:
-		"""One level of tower"""
-		def __init__(self, width: int = 1, pos: int = 0):
-			self._width = width
-			self.pos = pos
+    def __init__(
+            self,
+            max_level: int = 3,
+            state_matrix: List[List[int]] = None,
+            solved: bool = False
+        ):
+        """Create state of tower
+        :param max_level: count of blocks of tower
+        :param state_matrix: max_level * _TOWERS_COUNT matrix with size of blocks
+        if now specified, matrix will be created using max_level value
+        :param solved: if True matrix and no state_matrix sqeified,
+        state_matrix will be creted as already solved
+        """
+        assert max_level > 0, f'Illegal number of levels ({max_level})'
+        self._max_level = max_level
+        if state_matrix is not None:
+            blocks = set(item for row in state_matrix for item in row)
+            assert blocks == set(range(self._max_level + 1)), \
+                f'Invalid state matrix: {state_matrix}'  # set of blocks inconsistent
+            assert all(list(col) == sorted(col) for col in zip(*state_matrix)), \
+                f'Invalid state matrix: {state_matrix}'  # matrix is not sorted vertically
+            assert sum(1 for row in state_matrix for item in row if item) == self._max_level, \
+                f'Invalid state matrix: {state_matrix}'  # count of blocks inconsistent
+            self._state_matrix = state_matrix
+        else:  # build state with one tower
+            tower_idx = State._TOWERS_COUNT - 1 if solved else 0  # if solved populate last tower
+            self._state_matrix = []
+            for row in range(self._max_level):
+                level = []
+                for col in range(State._TOWERS_COUNT):
+                    level.append(row + 1 if col == tower_idx else 0)
+                self._state_matrix.append(level)
+                
+    def __str__(self):
+        """State representation
+        Sample output for State():
+           [||]        ||         ||     
+          [ || ]       ||         ||     
+         [  ||  ]      ||         ||     
+        =================================
+        """
+        output = ''
+        tower_space = State._TOWERS_MARGIN + self._max_level * 2 + 4
+        for row in self._state_matrix:
+            for block in row:
+                layer = f'[{" "*block}||{" "*block}]' if block else '||'
+                output += f'{layer:^{tower_space}}'
+            output += '\n'
+        output += '=' * tower_space * State._TOWERS_COUNT
+        return output
 
-		@property
-		def width(self):
-			return self._width
-
-		def __str__(self):
-			w = self._width
-			return f'[{" "*w}||{" "*w}]'
-
-		def __lt__(self, right: 'Layer') -> bool:
-			return self._width < right.width
-
-		def __eq__(self, right: int) -> bool:
-			return self._width == right
-
-	def __init__(
-		self,
-		max_level: int = 3,
-		towers: List[List[int]] = None,
-		solved: bool = False
-	):
-		assert max_level > 0, f'Illegal number of levels ({max_level})'
-		self._max_level = max_level
-		self._towers = [[0 for _ in range(self._max_level)] 
-							for _ in range(State._TOWERS_COUNT)]
-		if towers is not None:
-			layers = set(layer for tower in towers for layer in tower)
-			assert layers == set(range(1, self._max_level + 1)), \
-				f'Illegal layers: {layers}'
-			for tower_idx, tower in enumerate(towers):
-				for width_idx, width in enumerate(sorted(tower, reverse=True)):
-					width_idx = self._max_level - width_idx - 1  # going backwards
-					self._towers[tower_idx][width_idx] = State.Layer(width)
-		else:
-			for idx in range(self._max_level):
-				if solved:  # populate only last tower
-					self._towers[-1][idx] = State.Layer(idx)
-				else:  # populate only first tower
-					self._towers[0][idx] = State.Layer(idx)
-
-	def __str__(self):
-		"""State representation
-		Sample output for State():
-		   [||]        ||         ||     
-		  [ || ]       ||         ||     
- 		 [  ||  ]      ||         ||     
-		=================================
-		"""
-		output = ''
-		tower_space = State._TOWERS_MARGIN + self._max_level * 2 + 4
-		for level in range(self._max_level):
-			for tower in range(State._TOWERS_COUNT):
-				item = self._towers[tower][level]
-				output += f'{str(item) if item else "||":^{tower_space}}'
-			output += '\n'
-		output += '=' * tower_space * State._TOWERS_COUNT
-		return output
-
-	def move(self, src: int, dst: int) -> None:
-		"""Moves top level from src to dst
-		:param src: where to pick from
-		:param dst: where to place at
-		"""
-		self._towers[src]
+    def move(self, src: int, dst: int) -> None:
+        """Moves top level from src to dst
+        :param src: where to pick from
+        :param dst: where to place at
+        """
+        pass
 
 def main():
-	obj1 = State()
-	print(obj1)
-	print(State(solved=True))
-	print(State(max_level=5))
-	print(State(max_level=3, towers=[[2, 1], [3], []]))
-	print(State(max_level=6, towers=[[5, 3, 6, 4, 1, 2], [], []]))
-	
+    obj1 = State()
+    print(obj1)
+    print(State(solved=True))
+    print(State(max_level=5))
+    print(State(
+        max_level=3,
+        state_matrix=[
+            [0, 0, 0],
+            [1, 0, 0],
+            [3, 2, 0]]))
+
 
 if __name__ == '__main__':
-	main()
+    main()
